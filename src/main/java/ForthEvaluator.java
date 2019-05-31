2efdcdc560f7bb3c0cc346d97d409cb4a967e16b
mivanchenko <mivanchenko@cfmc.ru>
2019-05-31 17:37:14 +0300

    2019-05-31 first commit

--- src/main/java/ForthEvaluator.java
+++ src/main/java/ForthEvaluator.java
@@ -1,12 +1,20 @@
 import java.util.List;
 import java.util.Stack;
+import java.util.stream.IntStream;
 
 public class ForthEvaluator
 {
-    private Stack<Integer> stack = new Stack<>( );
+    private Stack<Integer> stackValues     = new Stack<>( );
+    private Stack<Integer> stackOperations = new Stack<>( );
 
-    public List<Integer> evaluateProgram( List<String> singletonList )
+    public List<Integer> evaluateProgram( List<String> list )
     {
+        this.parseOperations( list );
         return null;
+    }
+
+    private void parseOperations( List<String> list )
+    {
+
     }
 }
