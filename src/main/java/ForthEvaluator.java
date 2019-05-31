b3cb0253de8820cad8300efdc46c026a4a8192a0
mivanchenko <mivanchenko@cfmc.ru>
2019-05-31 17:04:08 +0300

    2019-05-31 first commit

---
+++ src/main/java/ForthEvaluator.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.Stack;
+
+public class ForthEvaluator
+{
+    private Stack<Integer> stack = new Stack<>( );
+
+    public List<Integer> evaluateProgram( List<String> singletonList )
+    {
+        return null;
+    }
+}
